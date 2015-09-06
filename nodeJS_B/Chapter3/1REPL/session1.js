7001518d1d33f629a736054eba17dba4f4f7c334
PriyaSilverback <Nathan@Nathans-MacBook.local>
2015-09-06 23:08:25 +1000

    n

---
+++ nodeJS_B/Chapter3/1REPL/session1.js
@@ -0,0 +1,5 @@
+function someRandomNumber() {
+console.log(parseInt(Math.random() * 1000));
+}
+someRandomNumber()
+someRandomNumber()
---
+++ nodeJS_B/Chapter3/1REPL/time.js
@@ -0,0 +1,4 @@
+function hello(fullname){
+console.log("Hi " + fullname + " The Time is :" + new Date().getHours() + "Hours" + new Date().getMinutes() + "Minutes!");
+}
+hello("priya")
